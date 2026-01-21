1f1a252dac3814d83a53ae4ec53f642fae8505c9
doobcontrol <dooacontrol@protonmail.com>
2026-01-22 05:49:32 +0800

    Feat: Implement database creation

--- xyDbMysql/xyDbMysql.go
+++ xyDbMysql/xyDbMysql.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/doobcontrol/gDb/xyDb"
 	_ "github.com/go-sql-driver/mysql"
+    "net/url"
 	"strings"
 )
 
@@ -14,47 +15,52 @@ type DbMysqlAccess struct{
    xyDb.DbAccess
 }
 func (dba *DbMysqlAccess) InitDb(initPars map[string]string, dbStructure xyDb.DbStructure) (string, error) {
-   dba.SetDriverName(S_mysqlDriverName)
+    dba.SetDriverName(S_mysqlDriverName)
 
-   adminConnectString, newDbConnectString := makeConnectString(initPars, dbStructure.DbName)
-   db, err := sql.Open(dba.DbDriverName, adminConnectString)
-   if err != nil {
-	   return "", err
-   }
-   defer db.Close() // Defer closing the database connection until the main function finishes
+   	adminConnectString, newDbConnectString := makeConnectString(initPars, dbStructure.DbName)
+   	db, err := sql.Open(dba.DbDriverName, adminConnectString)
+   	if err != nil {
+	   	return "", err
+   	}
 
-   //Create tables script
-   dScript, err := dba.DbScript(dbStructure)
-   if err != nil {
-	   return "", err
-   }
-
-   //Create Db script
-   var dbBuilder strings.Builder
-   dbBuilder.WriteString(fmt.Sprintf("CREATE DATABASE %s;", dbStructure.DbName))
-   dbBuilder.WriteString(fmt.Sprintf(
+   	//Create Db script
+   	var dbBuilder strings.Builder
+   	dbBuilder.WriteString(fmt.Sprintf("CREATE DATABASE %s;", dbStructure.DbName))
+   	dbBuilder.WriteString(fmt.Sprintf(
 	"CREATE USER '%s'@'localhost' IDENTIFIED BY '%s';", 
 	initPars[S_newName],
 	initPars[S_newPass],
 	))
-   dbBuilder.WriteString(fmt.Sprintf(
+    dbBuilder.WriteString(fmt.Sprintf(
 	"GRANT ALL PRIVILEGES ON %s.* TO '%s'@'localhost';", 
 	dbStructure.DbName,
 	initPars[S_newName],
 	))
-	dbBuilder.WriteString(dScript)
+	dbBuilder.WriteString("FLUSH PRIVILEGES;")
 
 	if _, err = db.Exec(dbBuilder.String()); err != nil {
 		return "", err
 	}
+	db.Close()
 
-   db, err = sql.Open(dba.DbDriverName, newDbConnectString)
-   if err != nil {
+   	db, err = sql.Open(dba.DbDriverName, newDbConnectString)
+   	if err != nil {
 	   return "", err
-   }
+   	}
 
-   dba.Db = db
-   return newDbConnectString, nil
+   	dba.Db = db
+
+   	//Create tables script
+   	dScript, err := dba.DbScript(dbStructure)
+   	if err != nil {
+	   	return "", err
+   	}
+
+	if _, err = db.Exec(dScript); err != nil {
+		return "", err
+	}
+	
+   	return newDbConnectString, nil
 }
 
 const S_adminName = "adminName"
@@ -63,14 +69,13 @@ const S_newName = "newName" //the new user for new dbname
 const S_newPass = "newPass" //the new user's password for new dbname
 const S_protocol = "protocol"
 const S_address = "address"
-//const S_dbname = "dbname"
 const S_params = "params"
 func makeConnectString(initPars map[string]string, dbName string) (string, string) {
 	var username = initPars[S_adminName]
-	var password = initPars[S_adminPass]
+	var password = url.PathEscape(initPars[S_adminPass])
 	var protocol = "tcp"
 	var address = initPars[S_address]
-	var dbname = ""
+	var dbname = "mysql"
 	var params = ""
 
 	if value, ok := initPars[S_protocol]; ok {
@@ -91,7 +96,7 @@ func makeConnectString(initPars map[string]string, dbName string) (string, strin
 	)
 
 	username = initPars[S_newName]
-	password = initPars[S_newPass]
+	password = url.PathEscape(initPars[S_newPass])
 	dbname = dbName
 	newDbConnectString := fmt.Sprintf(
 		"%s:%s@%s(%s)/%s%s", 
